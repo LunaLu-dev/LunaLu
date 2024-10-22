6a75957f6731cc89d314e3cfa388e2b21c95bcf8
Luna Nordbergh <dev@lunalu.org>
2024-10-23 00:39:22 +0200

    Add main, pronouns, and socials components
    Introduced MainComponent with HTML, CSS, and unit tests. Created PronounsComponent and SocialsComponent with their respective templates and tests. These changes establish the basic structure and styling for the main profile page and related sections.

---
+++ src/app/pronouns/pronouns.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pronouns',
+  standalone: true,
+  imports: [],
+  templateUrl: './pronouns.component.html',
+  styleUrl: './pronouns.component.css'
+})
+export class PronounsComponent {
+
+}
