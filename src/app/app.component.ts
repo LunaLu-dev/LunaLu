3a72403352843bca2cda6004981edd32f2161543
Luna Nordbergh <dev@lunalu.org>
2024-10-23 00:37:30 +0200

    Add new images and simplify home page content
    Added `BG.webp` and `LunaLu.webp` to the `public/img` directory. Updated the `favicon.ico` with changes. Simplified the `app.component.html` content by removing placeholder HTML and embedded styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+import {MainComponent} from './main/main.component';
+import {PronounsComponent} from './pronouns/pronouns.component';
+import {SocialsComponent} from './socials/socials.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage, MainComponent, PronounsComponent, SocialsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {SocialsComponent} from './socials/socials.component';
+import {PronounsComponent} from './pronouns/pronouns.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    title: 'LunaLu'
+  },
+  {
+    path: 'socials',
+    component: SocialsComponent,
+    title: 'LunaLu'
+  },
+  {
+    path: 'pronouns',
+    component: PronounsComponent,
+  }
+];
