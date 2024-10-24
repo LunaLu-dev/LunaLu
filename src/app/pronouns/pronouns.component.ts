f97cb44bca6d6477fb6cad36e339630a43915fab
Luna Nordbergh <dev@lunalu.org>
2024-10-24 02:42:50 +0200

    Add social media and flag images, update UI/UX
    Added images for social media links and flags, and incorporated them into the updated UI/UX elements for profile and pronoun components. Enhanced the visuals with new CSS styles and JavaScript logic for time and age updates.

--- src/app/pronouns/pronouns.component.ts
+++ src/app/pronouns/pronouns.component.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-pronouns',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './pronouns.component.html',
   styleUrl: './pronouns.component.css'
 })
 export class PronounsComponent {
 
+  time: string = "";
+  age: string = "";
+
+  UpdateTime(){
+    //Update Clock
+    const date = new Date();
+    this.time = date.toLocaleTimeString("en-US", {hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: "Europe/Stockholm"});
+
+    //Update Age
+    const born = new Date("2004-04-24T02:31:00");
+
+    //Split Up date
+    let year: number = date.getFullYear() - born.getFullYear();
+    let month: number = date.getMonth() - born.getMonth();
+    let day: number = date.getDate() - born.getDate();
+    let hour: number = date.getHours() - born.getHours();
+    let minute: number = date.getMinutes() - born.getMinutes();
+    let second: number = date.getSeconds() - born.getSeconds();
+
+    if(second < 0){
+      second += 60;
+      minute--;
+    }
+    if(minute < 0){
+      minute += 60;
+      hour--;
+    }
+    if(hour < 0){
+      hour += 24;
+      day--;
+    }
+    if(day < 0){
+      let prevMonth = new Date(date.getFullYear(), date.getMonth(), 0,).getDate();
+      day += prevMonth;
+      month --;
+    }
+    if(month < 0){
+      month += 12;
+      year--;
+    }
+
+    this.age = `${year} years, ${month} months, ${day} days, ${hour} hours, ${minute} minutes and , ${second} seconds`;
+  }
+
+  ngOnInit() {
+    this.UpdateTime();
+    setInterval(() => {
+      this.UpdateTime()
+    },1000)
+  }
+
+  protected readonly window = window;
 }
--- src/app/socials/socials.component.ts
+++ src/app/socials/socials.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-socials',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './socials.component.html',
   styleUrl: './socials.component.css'
 })
 export class SocialsComponent {
 
+  OpenLink(index: number){
+    const links = [
+      'https://only-fans.uk/LunaLu',
+      'https://discord.com/users/1098996480011944017',
+      'https://www.twitch.tv/lunaluplayz',
+      'https://vrchat.com/home/user/usr_19d6e456-23a6-41d4-8d0a-cf0ddd305e47',
+      'https://www.reddit.com/user/LunaLu_04',
+      'https://github.com/LunaLu-dev',
+      'https://www.youtube.com/@Luna_Lu1',
+      'https://x.com/LunaLuplayz',
+      'https://www.planetminecraft.com/member/_luna04_/',
+      'https://www.pinterest.co.uk/lunaplayz04/',
+      'https://wishlist.lunalu.org/',
+      'https://steamcommunity.com/id/_LunaLu_/',
+      'https://open.spotify.com/user/31jo3k6nxhloleusg3tidy7ctimq?si=93424dcbf62e47ea',
+      'https://myanimelist.net/profile/LunaLuPlayz',
+      'mailto:luna.lu@lunalu.org',
+    ];
+
+    window.open(links[index]);
+  }
 }
